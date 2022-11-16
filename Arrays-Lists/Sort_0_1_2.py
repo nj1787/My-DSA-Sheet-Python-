02791dcb28b4a0dfeadce1de631f43f60ee2cc86
Neeraj Jain <neerajj9@gmail.com>
2022-11-16 22:37:23 +0530

    Create Sort_0_1_2.py
    Initiated the solution to the problem.

---
+++ Arrays-Lists/Sort_0_1_2.py
@@ -0,0 +1,16 @@
+#Given an array/list consisting of only 0's, 1's and 2's. Your task is to sort the given list.
+
+#Test Case 1 --> [1, 1, 2, 0, 2, 0, 0, 2, 0]
+#Test Case 2 --> [2, 2, 1, 1, 2, 1, 1, 2, 1]
+#Test Case 3 --> [1, 0, 0, 0, 0, 0, 2, 1, 1]
+
+#I have solved this problem on various platforms like Leetcode, GeeksForGeeks, Codestudio(Coding Ninjas). You can use the test cases available on those
+#portals.Both the approaches works fine for any test case.
+
+#Brute force approach
+
+def sort_0_1_2_bruteforce(input_list):
+  pass
+
+
+
